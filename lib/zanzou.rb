69926e6f566e8d83ad4b5cbdb62a92721caaa6f0
Yutaka HARA <yutaka.hara+github@gmail.com>
2019-06-27 22:04:36 +0900

    feat: Add some more

--- lib/zanzou.rb
+++ lib/zanzou.rb
@@ -1,25 +1,80 @@
 require "zanzou/version"
 
 module Zanzou
-  class ShadowNode < Object
-    def initialize(orig_node, parent:, parent_key:)
-      @orig_node, @parent, @parent_key = orig_node, parent, parent_key
-      @modified = false
-      @modifications = {}
+  class ShadowNode
+    IMMUTABLE_CLASSES = [
+      TrueClass, FalseClass, NilClass,
+      Symbol, Numeric
+    ]
+
+    def self.create(orig_obj, parent:, parent_key:)
+      case orig_obj
+      when Array
+        ArrayShadow.new(orig_obj, parent: parent, parent_key: parent_key)
+      when Hash
+        HashShadow.new(orig_obj, parent: parent, parent_key: parent_key)
+      else
+        if orig_obj.frozen? || IMMUTABLE_CLASSES.include?(orig_obj.class)
+          orig_obj
+        elsif orig_obj.respond_to?(:zanzou_class)
+          orig_obj.zanzou_class.new(orig_obj, parent: parent, parent_key: parent_key)
+        else
+          AnyObjectShadow.new(orig_obj, parent: parent, parent_key: parent_key)
+        end
+      end
     end
 
-    def method_missing(name, *args)
-      if name =~ /(.+)=/
-        handle_setter($1.to_sym, args[0])
+    def self.finalize(shadow)
+      orig_obj      = shadow.instance_variable_get(:@orig_obj)
+      modified      = shadow.instance_variable_get(:@modified)
+      new_obj       = shadow.instance_variable_get(:@new_obj)
+      modifications = shadow.instance_variable_get(:@modifications)
+      modifications.transform_values!{|v|
+        ShadowNode === v ? ShadowNode.finalize(v) : v
+      }
+
+      #pp cls: shadow.class.name, orig_obj: orig_obj, modified: modified, modifications: modifications, new_obj: new_obj
+      if modified
+        if new_obj
+          if modifications.empty?
+            ret = new_obj
+          else
+            ret = shadow.class.merge(new_obj, modifications)
+          end
+        else
+          if modifications.empty?
+            ret = orig_obj
+          else
+            ret = shadow.class.merge(orig_obj, modifications)
+          end
+        end
       else
-        handle_getter(name.to_sym)
+        ret = orig_obj
       end
+      #pp ret: ret
+      return ret
+    end
+
+    def initialize(orig_obj, parent:, parent_key:)
+      @orig_obj, @parent, @parent_key = orig_obj, parent, parent_key
+      @modified = false
+      @modifications = {}
+      @new_obj = nil
     end
 
     private
 
-    def handle_setter(key, value)
-      @modifications[key] = value
+    def handle_destructive_method_call(name, args)
+      modified!
+      @new_obj = @orig_obj.dup
+      return @new_obj.public_send(name, *args)
+    end
+
+    def handle_non_destructive_method_call(name, args)
+      return (@new_obj || @orig_obj).public_send(name, *args)
+    end
+
+    def modified!
       @modified = true
 
       # Mark ancestors to be modified
@@ -34,34 +89,104 @@ module Zanzou
         @parent.instance_variable_get(:@modifications)[@parent_key] = self
       end
     end
+  end
 
-    def handle_getter(key)
-      if @modifications.key?(key)
-        @modifications[key]
+  class HashShadow < ShadowNode
+    def self.merge(orig_hash, modifications)
+      orig_hash.merge(modifications)
+    end
+
+    def method_missing(name, *args)
+      case name
+      when :[]=
+        handle_setter(args[0], args[1])
+      when :[]
+        handle_getter(args[0])
       else
-        ShadowNode.new(@orig_node.public_send(key), parent: self, parent_key: key)
+        handle_destructive_method_call(name, args)
       end
     end
 
-    def self.finalize(shadow)
-      orig_node = shadow.instance_variable_get(:@orig_node)
-      modified = shadow.instance_variable_get(:@modified)
-      modifications = shadow.instance_variable_get(:@modifications)
-      modifications.transform_values!{|v|
-        ShadowNode === v ? ShadowNode.finalize(v) : v
-      }
-      if modified
-        hash = orig_node.to_h.merge(modifications)
-        ret = orig_node.class.new(hash)
+    private
+
+    def handle_setter(key, value)
+      modified!
+      @modifications[key] = value
+      return value
+    end
+
+    def handle_getter(key)
+      if @modifications.key?(key)
+        return @modifications[key]
       else
-        ret = orig_node
+        return ShadowNode.create(@orig_obj.public_send(key), parent: self, parent_key: key)
       end
-      return ret
+    end
+  end
+
+  class ArrayShadow < ShadowNode
+    def self.merge(orig_ary, modifications)
+      ret = orig_ary.dup
+      modifications.each{|k, v| ret[k] = v}
+      ret
+    end
+
+    def initialize(*args)
+      super
+      @children = {}
+    end
+
+    def method_missing(name, *args)
+      case name
+      when :[]=
+        handle_setter(args[0], args[1])
+      when :[]
+        handle_getter(args[0])
+      else
+        handle_destructive_method_call(name, args)
+      end
+    end
+
+    private
+
+    def handle_setter(idx, value)
+      modified!
+      @new_obj = @orig_obj.dup
+      @new_obj[idx] = value
+      return value
+    end
+
+    def handle_getter(idx)
+      if @new_obj
+        return @new_obj[idx]
+      else
+        return ShadowNode.create(@orig_obj[idx], parent: self, parent_key: idx)
+      end
+    end
+
+    def handle_destructive_method_call(name, args)
+      modified!
+      @new_obj = @orig_obj.dup
+      return @new_obj.public_send(name, *args)
+    end
+  end
+
+  # Shadow for any Ruby objects (except container objects, which needs 
+  # special Shadow class to handle parent-child relationship).
+  # We know nothing about the class, so assume all methods are
+  # destructive (pessimistic)
+  class AnyObjectShadow < ShadowNode
+    def method_missing(name, *args)
+      return handle_destructive_method_call(name, args)
     end
   end
 
   def with_updates(&block)
-    shadow = ShadowNode.new(self, parent: nil, parent_key: nil)
+    Zanzou.with_updates(self, &block)
+  end
+
+  def self.with_updates(obj, &block)
+    shadow = ShadowNode.create(obj, parent: nil, parent_key: nil)
     block.call(shadow)
     return ShadowNode.finalize(shadow)
   end
--- spec/zanzou_spec.rb
+++ spec/zanzou_spec.rb
@@ -2,13 +2,31 @@ require 'zanzou'
 
 class HashLike
   include Zanzou
+
+  class HashLikeShadow < Zanzou::HashShadow
+    def self.merge(orig_hashlike, modifications)
+      HashLike.new(orig_hashlike.to_h.merge(modifications))
+    end
+
+    def method_missing(name, *args)
+      if name =~ /(.+)=/
+        handle_setter($1.to_sym, args[0])
+      else
+        handle_getter(name)
+      end
+    end
+  end
+
+  def zanzou_class
+    HashLikeShadow
+  end
   
   def initialize(hash={})
     @hash = hash
   end
 
   def respond_to?(key)
-    @hash.key?(key)
+    key == :zanzou_class || @hash.key?(key)
   end
 
   def method_missing(name, *args)
@@ -25,30 +43,77 @@ class HashLike
 end
 
 describe "Zanzou#with_updates" do
-  it "should not change the original" do
-    orig = HashLike.new(a: 1, b: HashLike.new(c: 3, d: 4))
-    new_obj = orig.with_updates{|o|
-      o.b.d = 5
-    }
+  context "HashLike" do
+    it "should not change the original" do
+      orig = HashLike.new(a: 1, b: HashLike.new(c: 3, d: 4))
+      new_obj = orig.with_updates{|o|
+        o.b.d = 5
+      }
 
-    expect(orig.b.d).to eq(4)
-    expect(new_obj.b.d).to eq(5)
+      expect(orig.b.d).to eq(4)
+      expect(new_obj.b.d).to eq(5)
+    end
+
+    it "should return the object if no change is made" do
+      orig = HashLike.new
+      new_obj = orig.with_updates{}
+
+      expect(new_obj).to be(orig)
+    end
+
+    it "should not duplicate unchanged objects" do
+      orig = HashLike.new(a: 1, b: [])
+      new_obj = orig.with_updates{|o| o.a = 2}
+
+      expect(new_obj.b).to be(orig.b)
+    end
   end
 
-  it "should return the object if no change is made" do
-    orig = HashLike.new
-    new_obj = orig.with_updates{}
+  context "Array" do
+    it "destructive method does not modify the original" do
+      orig = [1,2,3]
+      new_obj = Zanzou.with_updates(orig){|o| o.pop}
 
-    expect(new_obj).to be(orig)
+      expect(orig).to eq([1,2,3])
+      expect(new_obj).to eq([1,2])
+    end
+
+    it "#[]=" do
+      orig = [1,[2,3],4]
+      new_obj = Zanzou.with_updates(orig){|o| o[1][1] = 99}
+
+      expect(orig).to eq([1,[2,3],4])
+      expect(new_obj).to eq([1,[2,99],4])
+    end
   end
 
-  it "should not duplicate unchanged objects" do
-    orig = HashLike.new(a: 1, b: [])
-    new_obj = orig.with_updates{|o| o.a = 2}
+  context "Hash" do
+    it "destructive method does not modify the original" do
+      orig = HashLike.new(a: 1, b: {c: 1})
+      new_obj = orig.with_updates{|o| o.b.shift}
 
-    expect(new_obj.b).to be(orig.b)
+      expect(orig.b).to eq({c: 1})
+      expect(new_obj.b).to eq({})
+    end
+
+    it "#[]=" do
+      orig = HashLike.new(a: 1, b: {c: 1})
+      new_obj = orig.with_updates{|o| o.b[:c] = 99}
+
+      expect(orig.b).to eq({c: 1})
+      expect(new_obj.b).to eq({c: 99})
+    end
+  end
+
+  context "String" do
+    it "destructive method does not modify the original" do
+      orig = HashLike.new(s: "hi")
+      new_obj = orig.with_updates{|o| o.s.upcase!}
+
+      expect(orig.s).to eq("hi")
+      expect(new_obj.s).to eq("HI")
+    end
   end
 end
 
-# TODO: Array
 # TODO: HashWithIndifferentAccess
